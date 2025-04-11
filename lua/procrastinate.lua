e2d050276533142218ec66283079859a08bacca1
Masa Fukui <masahiro.m.fukui@gmail.com>
2025-04-11 23:57:42 +0900

    feat: use vim.bo.commentstring

--- lua/procrastinate.lua
+++ lua/procrastinate.lua
@@ -1,26 +1,27 @@
+
+local function split_commentstring()
+    local commentstring = vim.bo.commentstring
+
+    if not commentstring or commentstring == "" then
+        return "", ""
+    end
+
+    local prefix, suffix = commentstring:match("^(.-)%%s(.-)$")
+
+    if not prefix:match("%s$") then
+        prefix = prefix .. " "
+    end
+
+    if suffix == nil then
+        suffix = ""
+    end
+
+    return prefix, suffix
+end
+
 local M = {}
 
-local default_config = {
-	file_type_comment_chars_map = {
-		lua = "--",
-		sh = "#",
-		go = "//",
-		rust = "//",
-		python = "#",
-		c = "//",
-		cpp = "//",
-		js = "//",
-		css = "/*",
-		scss = "/*",
-	},
-}
-
-local config = {}
-
-M.setup = function(user_config)
-	-- Merge user config with default config
-	config = vim.tbl_deep_extend("force", default_config, user_config or {})
-
+M.setup = function()
 	-- Automatically create a Vim command to call insert_todo_comment
 	vim.api.nvim_create_user_command("InsertTodo", function()
 		require("procrastinate").insert_todo_comment()
@@ -30,17 +31,15 @@ end
 M.insert_todo_comment = function()
 	local current_line = vim.api.nvim_get_current_line()
 	local cursor_pos = vim.api.nvim_win_get_cursor(0) -- {line, col}
-
-	-- Get the current file type
-	local current_file_type = vim.bo.filetype
-	local comment_id = config.file_type_comment_chars_map[current_file_type] or "--" -- default to "--" if no match
+    local prefix, _ = split_commentstring()
 
 	-- Insert TODO comment at the current line and move cursor to the end of it
-	local updated_line = current_line .. comment_id .. " TODO:  "
+	local updated_line = current_line .. prefix .. " TODO:  "
 	vim.api.nvim_set_current_line(updated_line)
 
 	-- Move the cursor to the position right after "TODO: "
 	vim.api.nvim_win_set_cursor(0, { cursor_pos[1], #updated_line })
+
 	-- Enter insert mode
 	vim.cmd("startinsert")
 end
